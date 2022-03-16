c4cc7650e098fd73e510fb1097d0ab36d03cf9bf
TrunksJason <40633829+TrunksJason@users.noreply.github.com>
2022-03-16 17:04:31 +0700

    it is empty

--- src/FirstDemoPrj.Application/TaskAppService.cs
+++ src/FirstDemoPrj.Application/TaskAppService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Abp.Domain.Entities.Auditing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace FirstDemoPrj
 {
-    class TaskAppService
+
+    public class GetAllTasksInput
     {
+        public TaskState? State { get; set; }
+    }
+
+    [AutoMapFrom(typeof(Task))]
+    public class TaskListDto : EntityDto, IHasCreationTime
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public TaskState State { get; set; }
+    }
+    public interface ITaskAppService : IApplicationService
+    {
+        Task<ListResultDto<TaskListDto>> GetAll(GetAllTasksInput input);
     }
 }
